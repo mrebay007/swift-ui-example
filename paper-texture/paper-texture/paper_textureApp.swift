eb2238d3abc0d25018fb0bd8b888000c788f6d75
Eberhardt Macbook Air <eberhardtmacbookair@Eberhardts-MacBook-Air.local>
2024-08-04 21:21:22 -0600

    Initial Commit

---
+++ paper-texture/paper-texture/paper_textureApp.swift
@@ -0,0 +1,17 @@
+//
+//  paper_textureApp.swift
+//  paper-texture
+//
+//  Created by Eberhardt Macbook Air on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct paper_textureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paper-texture/paper-textureUITests/paper_textureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  paper_textureUITestsLaunchTests.swift
+//  paper-textureUITests
+//
+//  Created by Eberhardt Macbook Air on 8/4/24.
+//
+
+import XCTest
+
+final class paper_textureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
