82e3571c6129025bb0fc0d0dcd4b88f7b04873e0
Eberhardt Macbook Air <eberhardtmacbookair@Eberhardts-MacBook-Air.local>
2024-08-06 10:08:07 -0600

    Reader view for example one.

--- paper-texture/paper-texture/ExampleOneView.swift
+++ paper-texture/paper-texture/ExampleOneView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct ExampleOneView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.gray)
+                    .frame(maxWidth: 64, maxHeight: 64)
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Gradient(colors: [.pink, .white]))
+                    .frame(maxWidth: 64, maxHeight: 64)
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.gray)
+                    .frame(maxWidth: 64, maxHeight: 64)
+            }
+            
+            Text("Lorem Ipsum Dolor Set")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .fontDesign(.serif)
+                .frame(width: 256)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .font(.system(size: 18.0))
+                .fontDesign(.serif)
+                .frame(width: 256)
+        }
+        
     }
 }
 
